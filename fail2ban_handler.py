a9a3175a89597522f866d47749da4ebc5214395c
crimike <crimike@example.com>
2021-01-23 12:35:50 +0100

    Fail2ban handler

---
+++ fail2ban_handler.py
@@ -0,0 +1,67 @@
+import telegram
+from watchdog.events import FileSystemEventHandler
+import logging
+
+class Fail2BanEventHandler(FileSystemEventHandler):
+    
+    def __init__(self, config, notify_function):
+        super().__init__()
+        self.path = config['log_path']
+        self.tell = 0
+        self.notify = notify_function
+        self.logger = logging.getLogger(__name__)
+        #self.logging = logging.getLogger(__name_)
+        #self.logging.basicConfig(
+
+    def parse_log(self, log):
+        self.logger.info("Handling information from the log")
+        for line in log.split('\n'):
+            self.logger.debug(line)
+            if '[sshd]' in line:
+                # SSH event
+                self.logger.info("SSH event encountered")
+                evt = ''.join(line.split(':')[3:])
+                self.notify(evt)
+
+    def dispatch(self, event):
+        if event.src_path == self.path:
+            self.logger.debug("Dispatching modified event for " + event.src_path)
+            super().dispatch(event)
+
+    def on_modified(self, event):
+        self.logger.info("Modification event is being handled for " + event.src_path)
+        f = open(event.src_path,'r')
+        if self.tell == 0:
+            # new file, parse last line
+            self.logger.info("First time a modification was recorded for this file, grabbing last line")
+            try:
+                log = f.readlines()[-1]
+            except:
+                log = ""
+            self.parse_log(log)
+            self.tell = f.tell()
+            self.logger.debug("Setting new tell to " + str(self.tell))
+        else:
+            f.read()
+            new_tell = f.tell()
+            self.logger.debug("File length is " + str(new_tell))
+            if new_tell < self.tell:
+                # new file is shorter than previous - logrotate for example
+                self.logger.debug("New file is shorter than previous, parsing everything")
+                f.seek(0)
+                log = f.read()
+                self.parse_log(log)
+                self.tell = new_tell
+                self.logger.debug("Setting new tell to " + str(new_tell))
+            else:
+                # content was added to the file
+                self.logger.debug("Content was added to " + event.src_path)
+                f.seek(self.tell)
+                log = f.read()
+                self.parse_log(log)
+                self.tell = f.tell()
+                self.logger.debug("Setting new tell to " + str(self.tell))
+
+        f.close()
+
+
