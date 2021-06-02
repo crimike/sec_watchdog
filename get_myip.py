ebfb51e1a917f2de70e38d756babe931098e2e89
crimike <crimike@example.com>
2021-06-02 16:23:29 +0200

    Getting IP

--- get_myip.py
+++ get_myip.py
@@ -1,6 +1,7 @@
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import logging
+import requests
 
 
 class GetMyIp():
@@ -9,9 +10,14 @@ class GetMyIp():
         self.chat_id = chat_id
         self.logger = logging.getLogger(__name__)
 
+    def getIp():
+        headers = {'User-Agent' : 'curl/7.68.0'}
+        r = requests.get("http://ifconfig.co", headers = headers)
+        return r.text
+
 
     def handler(self, update: Update, _: CallbackContext) -> None:
         if str(update.message.chat_id) != self.chat_id:
             self.logger.warning("Different chat tried to access this command")
         else:
-            update.message.reply_text("My ip is")
+            update.message.reply_text("My ip is " + getIp())
